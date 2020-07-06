4f73302d5ae1a66a8dbd91c8e071ae63ca2a7346
Vinsant <vinsantsvetoslavovsavov@gmail.com>
2020-07-07 00:02:36 +0300

    Exercise number 1 - basic console calculator

---
+++ Basic-Console-Calculator/CalculatorApp/CalculatorApp/Calculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            this.Result = 0;
+        }
+
+        public double Result { get; private set; }
+
+        public void Add(double number)
+        {
+            this.Result += number;
+        }
+
+        public void Add(double firstNum, double secondNum)
+        {
+            this.Result = firstNum + secondNum;
+        }
+
+        public void Subtract(double number)
+        {
+            this.Result -= number;
+        }
+
+        public void Subtract(double firstNum, double secondNum)
+        {
+            this.Result = firstNum - secondNum;
+        }
+
+
+        public void Multiply(double number)
+        {
+            this.Result *= number;
+        }
+
+        public void Multiply(double firstNum, double secondNum)
+        {
+            this.Result = firstNum * secondNum;
+        }
+
+        public void Division(double number)
+        {
+            if (number == 0)
+            {
+                throw new InvalidOperationException("Division by zero is not permitted");
+            }
+
+            this.Result /= number;
+        }
+
+        public void Division(double firstNum, double secondNum)
+        {
+            if (secondNum == 0)
+            {
+                throw new InvalidOperationException("Division by zero is not permitted");
+            }
+
+            this.Result = firstNum / secondNum;
+        }
+
+        public double FinalResult()
+        {
+            return this.Result;
+        }
+
+        public void Clear()
+        {
+            this.Result = 0;
+        }
+    }
+}
---
+++ Basic-Console-Calculator/CalculatorApp/CalculatorApp/StartUp.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+
+namespace CalculatorApp
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            while (true)
+            {
+                try
+                {
+                    string input = Console.ReadLine();
+
+                    if (input == "=")
+                    {
+                        Console.WriteLine($"The result is: {calculator.FinalResult()}");
+                    }
+                    else if (input == "+" || input == "-" || input == "*" || input == "/")
+                    {
+                        StartWithOperator(calculator, input);
+                    }
+                    else
+                    {
+                        NumberProcessing(calculator, input);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static void NumberProcessing(Calculator calculator, string input)
+        {
+            calculator.Clear();
+
+            double firstNum = double.Parse(input);
+            string operation = Console.ReadLine();
+            double secondNum = double.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case "+":
+                    calculator.Add(firstNum, secondNum);
+                    break;
+                case "-":
+                    calculator.Subtract(firstNum, secondNum);
+                    break;
+                case "*":
+                    calculator.Multiply(firstNum, secondNum);
+                    break;
+                case "/":
+                    calculator.Division(firstNum, secondNum);
+                    break;
+            }
+
+            Console.WriteLine($"The result is: {calculator.FinalResult()}");
+        }
+
+        private static void StartWithOperator(Calculator calculator, string input)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case "+":
+                    calculator.Add(firstNumber);
+                    break;
+                case "-":
+                    calculator.Subtract(firstNumber);
+                    break;
+                case "*":
+                    calculator.Multiply(firstNumber);
+                    break;
+                case "/":
+                    calculator.Division(firstNumber);
+                    break;
+            }
+
+            Console.WriteLine($"The result is: {calculator.FinalResult()}");
+        }
+    }
+}
